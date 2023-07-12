7177f16d87eacfd95aa2421b54a8d5bfb60269e2
abdelrahmantarek <abdelrahman.t.mahdy@gmail.com>
2023-07-12 13:22:53 +0300

    with lookups

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,9 +40,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "83133df5b38a0312877f58e228d7f914",
-"/": "83133df5b38a0312877f58e228d7f914",
-"main.dart.js": "57f10307511c6c2adb09f02091f6e86c",
+"index.html": "f7173b12af8a64ccb9eab07520651e19",
+"/": "f7173b12af8a64ccb9eab07520651e19",
+"main.dart.js": "9ba52fa74a4802db7941a97f5b39cdc5",
 "manifest.json": "e1cc2613f88e81285a011028f465988b",
 "version.json": "4c4c9cf980dc4e4548b529ef57125dfc"
 };
